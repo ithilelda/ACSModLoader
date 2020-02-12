3d9d5dd5a08a691cf03d48071715efd456ad8142
ithilelda <huawei_007@yahoo.com>
2020-02-12 17:51:07 +0800

    updated mltest to test preloader patcher capability. success.

--- MLTest/MLTest.cs
+++ MLTest/MLTest.cs
@@ -1,5 +1,8 @@
-﻿using ModLoader;
+﻿using System;
+using System.Linq;
+using ModLoader;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 
 namespace MLTest
@@ -9,7 +12,13 @@ namespace MLTest
     {
         public static void Patch(AssemblyDefinition asm)
         {
-            
+            var luamgr = asm.MainModule.Types.First(t => t.FullName == "XiaWorld.LuaMgr");
+            var init = luamgr.Methods.First(m => m.Name == "Init");
+            var processor = init.Body.GetILProcessor();
+            var last = processor.Body.Instructions.Last();
+            var writeline = asm.MainModule.ImportReference(typeof(Console).GetMethod("WriteLine", new Type[] {typeof(string)}));
+            processor.InsertBefore(last, processor.Create(OpCodes.Ldstr, "MLTest in Action"));
+            processor.InsertBefore(last, processor.Create(OpCodes.Call, writeline));
         }
     }
 }
