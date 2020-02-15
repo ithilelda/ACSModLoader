db37265cc33f892479c08fc0c24843ac6089a1c1
ithilelda <huawei_007@yahoo.com>
2020-02-15 23:39:18 +0800

    fixes assembly loading.

--- ModLoaderLite/ModLoaderLite.cs
+++ ModLoaderLite/ModLoaderLite.cs
@@ -12,7 +12,6 @@ namespace ModLoaderLite
     public class ModLoaderLite
     {
         private static Dictionary<string, bool> patched = new Dictionary<string, bool>();
-        private static bool initialized;
         private static Dictionary<string, Assembly> loadedAssemblies = new Dictionary<string, Assembly>();
 
         static ModLoaderLite()
@@ -21,11 +20,6 @@ namespace ModLoaderLite
         }
         public void Load(string path, string assemblyName="", string typeFullName="")
         {
-            if(!initialized)
-            {
-                Init();
-                initialized = true;
-            }
             if(!patched.TryGetValueOrDefault(path))
             {
                 HarmonyLoaderLite.Enter(path, assemblyName, typeFullName);
@@ -33,30 +27,19 @@ namespace ModLoaderLite
             }
         }
 
-        private void Init()
-        {
-            var thisDir = Assembly.GetExecutingAssembly().Location;
-            var thisDlls = Directory.GetFiles(thisDir, "*.dll", SearchOption.AllDirectories);
-            foreach(var dllFile in thisDlls)
-            {
-                if(Path.GetFileNameWithoutExtension(dllFile) != "ModLoaderLite")
-                {
-                    try
-                    {
-                        var bytes = File.ReadAllBytes(dllFile);
-                        var asm = Assembly.Load(bytes);
-                        loadedAssemblies.Add(asm.FullName, asm);
-                    }
-                    catch(Exception ex)
-                    {
-                        KLog.Dbg(ex.Message);
-                    }
-                }
-            }
-        }
         private static Assembly HandleAssemblyResolve(object sender, ResolveEventArgs arg)
         {
-            loadedAssemblies.TryGetValue(arg.Name, out var ret);
+            if(!loadedAssemblies.TryGetValue(arg.Name, out var ret))
+            {
+                var name = new AssemblyName(arg.Name).Name + ".dll";
+                var thisDir = Assembly.GetExecutingAssembly().Location;
+                var askedFile = Path.Combine(thisDir, name);
+                if(File.Exists(askedFile))
+                {
+                    var asm = Assembly.LoadFrom(askedFile);
+                    loadedAssemblies.Add(arg.Name, asm);
+                }
+            }
             return ret;
         }
     }
